5910e7cb18badb2b35560beffd86a8eeae92b642
Mario Recinos <lmrecinoshr@gmail.com>
2025-10-11 09:48:48 -0600

    Initial Commit

---
+++ Fakestagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fakestagram
+//
+//  Created by Luis Mario Recinos Hernández  on 11/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
